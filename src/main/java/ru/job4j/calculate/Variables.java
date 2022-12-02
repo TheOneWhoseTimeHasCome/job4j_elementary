a95f98dc2c098d24d5f271c76fb9e9d02b5f50db
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2022-12-03 02:36:53 +0500

    0. Что такое переменная. [#3610 #338693]

---
+++ src/main/java/ru/job4j/calculate/Variables.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+    }
+}
