62d8cbe741f413bd7ef99e22fbd5d981e7e2c4d1
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2022-12-06 21:46:52 +0500

    4.2. Тернарное сравнение [#189 #368270]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,22 @@ public class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax5To1Then5() {
+        int left = 5;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax3To3Then3() {
+        int left = 3;
+        int right = 3;
+        int result = Max.max(left, right);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
 }
