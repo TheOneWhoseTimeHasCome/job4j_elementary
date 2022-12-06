17c8a1907b6e4f516dda791743c8fbed420891fa
TheOneWhoseTimeHasCome <cadet211@gmail.com>
2022-12-06 20:24:48 +0500

    4.1.2. Глупый бот [#31323 #367802]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -2,15 +2,12 @@ package ru.job4j.condition;
 
 public class DummyBot {
     public static String answer(String question) {
-        String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            rsl = "Hi, SmartAss.";
-            return rsl;
+            return "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
-            rsl = "See you later.";
-            return rsl;
+            return "See you later.";
         }
-        return rsl;
+        return "I don't know. Please, ask another question.";
     }
 
     public static void main(String[] args) {
